cc35031b513b6edb582f83ea0e38c4fe4aa28d7c
Matvey Zabolotskikh <zabolotskih03@mail.ru>
2025-07-17 22:29:43 +0500

    4_try

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -11,13 +11,12 @@ import (
 )
 
 func ReturnHTML(w http.ResponseWriter, r *http.Request) {
-	html := "../index.html"
+	html := "./index.html"
 	if _, err := os.Stat(html); os.IsNotExist(err) {
 		http.Error(w, "Файл не найден", http.StatusNotFound)
 		return
 	}
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	w.WriteHeader(http.StatusOK)
 	http.ServeFile(w, r, html)
 }
 
@@ -41,6 +40,6 @@ func UploadHTML(w http.ResponseWriter, r *http.Request) {
 		defer fileRes.Close()
 		w.Header().Set("Content-Type", "text/html; charset=utf-8")
 		w.WriteHeader(http.StatusOK)
-		fileRes.WriteString(converString)
+		_, err = w.Write([]byte(converString))
 	}
 }
