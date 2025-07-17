34c7bf75b785fd805b6c0f589b7315edcfc0be40
Onirl <162680968+Onirl@users.noreply.github.com>
2025-07-17 22:33:05 +0500

    Revert "Six sprint first try"

--- cmd/main.go
+++ cmd/main.go
@@ -1,14 +1,5 @@
 package main
 
-import (
-	"log"
-	"os"
-
-	"github.com/Yandex-Practicum/go1fl-sprint6-final/internal/server"
-)
-
 func main() {
-	loger := log.New(os.Stdout, "INFO: ", log.Ldate|log.Ltime)
-	myServer := server.CreateServer(loger)
-	loger.Fatal(myServer.HttpServer.ListenAndServe())
+
 }
